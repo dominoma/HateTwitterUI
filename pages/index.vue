765521103e4721f6bef958fd8ad0a4e48e099357
dominoma <dominik.m.sander@t-online.de>
2020-02-26 18:47:00 +0100

    added 3 charts

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,127 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        HateTwitterUI
-      </h1>
-      <h2 class="subtitle">
-        My spectacular Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <b-card no-body>
+    <b-tabs card>
+      <b-tab title="Tweets insgesamt" lazy>
+        <b-card-title>
+          Anzahl der Tweets mit dem Hashtag
+          <code>#{{ tweetsChart.hashtag }}</code> pro Tag
+        </b-card-title>
+        <b-card-text>
+          <D3LineChart
+            ref="chart1"
+            :config="tweetsChart.config"
+            :datum="tweetsChart.data"
+          ></D3LineChart>
+        </b-card-text>
+      </b-tab>
+      <b-tab title="Tweets nach Sprachen" lazy>
+        <b-card-title>
+          Anzahl der Tweets mit dem Hashtag
+          <code>#{{ langChart.hashtag }}</code> nach Sprachen
+        </b-card-title>
+        <b-card-text>
+          <D3LineChart
+            :config="langChart.config"
+            :datum="langChart.data"
+          ></D3LineChart>
+        </b-card-text>
+      </b-tab>
+      <b-tab title="Sprachen" lazy>
+        <b-card-title>
+          Verteilung der Tweets mit dem Hashtag
+          <code>#{{ langDistChart.hashtag }}</code> nach Sprachen
+        </b-card-title>
+        <b-card-text>
+          <D3PieChart
+            :config="langDistChart.config"
+            :datum="langDistChart.data"
+          ></D3PieChart>
+        </b-card-text>
+      </b-tab>
+    </b-tabs>
+  </b-card>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import { D3LineChart, D3PieChart } from 'vue-d3-charts'
 
-export default {
+import hashtag from './hashtag.json'
+
+@Component({
   components: {
-    Logo
+    D3LineChart,
+    D3PieChart
+  }
+})
+export default class Index extends Vue {
+  get langChart() {
+    const langs = Object.entries(hashtag.lang)
+      .sort((a, b) => b[1] - a[1])
+      .slice(0, 5)
+      .map(([key]) => key)
+    const data = Object.values(
+      hashtag.dateLang.reduce((acc, entry) => {
+        const val = acc[entry.date] || {
+          when: entry.date,
+          ...Object.fromEntries(langs.map((key) => [key, 0]))
+        }
+        val[entry.lang] = entry.value
+        acc[entry.date] = val
+        return acc
+      }, {} as { [key: string]: { [key: string]: number } })
+    )
+    const config = {
+      date: { key: 'when', inputFormat: '%Y-%m-%d', outputFormat: '%d.%m.' },
+      values: langs,
+      curve: 'curveMonotoneX',
+      color: {
+        scheme: 'schemeSet1'
+      }
+    }
+    return { data, config, hashtag: hashtag.hashtag }
+  }
+
+  get tweetsChart() {
+    const data = Object.entries(hashtag.date).map(([date, count]) => ({
+      when: date,
+      Tweets: count
+    }))
+    const config = {
+      date: { key: 'when', inputFormat: '%Y-%m-%d', outputFormat: '%d.%m.' },
+      values: ['Tweets'],
+      curve: 'curveMonotoneX'
+    }
+    return { data, config, hashtag: hashtag.hashtag }
+  }
+
+  get langDistChart() {
+    const others = Object.entries(hashtag.lang)
+      .map(([lang, value]) => ({
+        name: lang,
+        total: value
+      }))
+      .sort((a, b) => b.total - a.total)
+    const data = others.splice(0, 5)
+    const othersEntry = others.reduce(
+      (acc, el) => {
+        acc.total += el.total
+        return acc
+      },
+      {
+        name: 'Andere',
+        total: 0
+      }
+    )
+    data.push(othersEntry)
+    const config = {
+      key: 'name',
+      value: 'total',
+      color: {
+        scheme: 'schemeSet1'
+      }
+    }
+    return { data, config, hashtag: hashtag.hashtag }
   }
 }
 </script>
--- vue-shim.d.ts
+++ vue-shim.d.ts
@@ -1,4 +1,11 @@
 declare module '*.vue' {
+  // eslint-disable-next-line import/no-duplicates
   import Vue from 'vue'
   export default Vue
+}
+declare module 'vue-d3-charts' {
+  // eslint-disable-next-line import/no-duplicates
+  import { VueConstructor } from 'vue'
+  export const D3LineChart: VueConstructor<Vue>
+  export const D3PieChart: VueConstructor<Vue>
 }
