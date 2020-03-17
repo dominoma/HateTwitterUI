e2878f6f298396ffffe475417787e42aa3dae194
dominoma <dominik.m.sander@t-online.de>
2020-03-17 18:13:13 +0100

    added doc

--- pages/hashtag/_hashtag.vue
+++ pages/hashtag/_hashtag.vue
@@ -7,6 +7,10 @@ import { Vue, Component } from 'vue-property-decorator'
 import { fetchHashtag } from '~/rest'
 import HashtagDashboard from '~/components/HashtagDashboard.vue'
 
+/**
+ * This class encapsulates the hashtag detail dashboard.
+ * It maps to the url /hashtag/:name
+ */
 @Component({
   components: {
     HashtagDashboard
